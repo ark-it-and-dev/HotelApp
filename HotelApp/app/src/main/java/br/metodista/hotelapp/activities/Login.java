3ca28d304bf81aea7c4ee0257931accbd9b4cf55
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-08-25 09:47:20 -0300

    + Adicionando activity_login.xml, Login.java e strings.xml

---
+++ HotelApp/app/src/main/java/br/metodista/hotelapp/activities/Login.java
@@ -0,0 +1,46 @@
+package br.metodista.hotelapp.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import br.metodista.hotelapp.R;
+
+public class Login extends Activity {
+
+    private Intent irPara;
+
+    private EditText txtLogin;
+    private EditText txtSenha;
+    private Button btnCancelar;
+    private Button btnEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        txtLogin = (EditText) findViewById(R.id.txtLogin);
+        txtSenha = (EditText) findViewById(R.id.txtSenha);
+        btnCancelar = (Button) findViewById(R.id.btnCancelar);
+        btnEntrar = (Button) findViewById(R.id.btnEntrar);
+
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+}
