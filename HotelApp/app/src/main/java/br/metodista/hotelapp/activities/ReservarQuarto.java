de8d43ee9adb78f7476249be5f732b4f5381800c
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-09-07 02:41:01 -0300

    + Adicao do layout e comportamentos da tela de reservar quarto

---
+++ HotelApp/app/src/main/java/br/metodista/hotelapp/activities/ReservarQuarto.java
@@ -0,0 +1,16 @@
+package br.metodista.hotelapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.metodista.hotelapp.R;
+
+public class ReservarQuarto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservar_quarto);
+    }
+
+}
