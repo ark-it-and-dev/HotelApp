714b702cd4dc9b94aa6cf3c58b9aa00f2e2ddf1a
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-09-07 02:29:50 -0300

    * Alteracao na tela e Activity Login

--- HotelApp/app/src/main/java/br/metodista/hotelapp/activities/Login.java
+++ HotelApp/app/src/main/java/br/metodista/hotelapp/activities/Login.java
@@ -1,20 +1,22 @@
 package br.metodista.hotelapp.activities;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import br.metodista.hotelapp.R;
+import helper.LoginHelper;
+import model.Usuario;
 
-public class Login extends Activity {
+public class Login extends AppCompatActivity {
+
+    private Usuario usuario;
+    private LoginHelper helper;
 
     private Intent irPara;
 
-    private EditText txtLogin;
-    private EditText txtSenha;
     private Button btnCancelar;
     private Button btnEntrar;
 
@@ -23,22 +25,43 @@ public class Login extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        txtLogin = (EditText) findViewById(R.id.txtLogin);
-        txtSenha = (EditText) findViewById(R.id.txtSenha);
-        btnCancelar = (Button) findViewById(R.id.btnCancelar);
+        helper = new LoginHelper(this);
         btnEntrar = (Button) findViewById(R.id.btnEntrar);
-
-        btnCancelar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+        btnCancelar = (Button) findViewById(R.id.btnCancelar);
 
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                irPara = new Intent(Login.this, Termos.class);
+//                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+//
+//                builder.setTitle("");
+//                builder.setMessage("Deseja manter os dados para conexão automática?");
+//
+//                builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        usuario = helper.getUsuarioDoLogin();
+//
+//                        UsuarioDAO dao = new UsuarioDAO(Login.this);
+//                        dao.inserir(usuario);
+//                        dao.close();
+//
+//                        irPara = new Intent(Login.this, Home.class);
+//                        startActivity(irPara);
+//                    }
+//                });
+//
+//                builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        irPara = new Intent(Login.this, Home.class);
+//                        startActivity(irPara);
+//                    }
+//                });
+//                AlertDialog alerta = builder.create();
+//                alerta.show();
+
+                irPara = new Intent(Login.this, Home.class);
                 startActivity(irPara);
             }
         });
