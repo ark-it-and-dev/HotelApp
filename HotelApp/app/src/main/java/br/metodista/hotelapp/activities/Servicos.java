805902f6ce53dc0fce29837a51a7d4accb040d63
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-09-24 09:10:14 -0300

    + ConclusÃao da tela de Servicos

---
+++ HotelApp/app/src/main/java/br/metodista/hotelapp/activities/Servicos.java
@@ -0,0 +1,128 @@
+package br.metodista.hotelapp.activities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+
+import br.metodista.hotelapp.R;
+import br.metodista.hotelapp.adapter.ExpandableListAdapter;
+
+public class Servicos extends AppCompatActivity {
+    private ExpandableListAdapter adapter;
+    private ExpandableListView expListView;
+    private List<String> listGroup;
+    private HashMap<String, List<String>> listChild;
+
+//    private ImageView imgAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servivos);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.expList);
+//        imgAdd = (ImageView) findViewById(R.id.imgAdd);
+
+        // preparing list data
+        prepareListData();
+
+        adapter = new ExpandableListAdapter(this, listGroup, listChild);
+
+        // setting list adapter
+        expListView.setAdapter(adapter);
+
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listGroup.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listGroup.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listGroup.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        listGroup.get(groupPosition) + " : " + listChild.get(listGroup.get(groupPosition)).get(childPosition),
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listGroup = new ArrayList<String>();
+        listChild = new HashMap<String, List<String>>();
+
+        listGroup.add("Pratos");
+        listGroup.add("Bebidas");
+
+        List<String> listChildPratos = new ArrayList<String>();
+        listChildPratos.add("Ribeye 380g " + " R$ 250,00");
+        listChildPratos.add("Conflit de Pimentões " + " R$ 70,00");
+        listChildPratos.add("Trop Kobe Beef-Baby beef c batata soufflés " + " R$ 240,00");
+        listChildPratos.add("The Burguer Lab Star " + " R$ 200,00");
+        listChildPratos.add("Moqueca de Camarão " + " R$ 80,00");
+        listChildPratos.add("Lagosta ao Thermidor " + " R$ 190,00");
+        listChildPratos.add("Camarão a Provençal " + " R$ 150,00");
+        listChildPratos.add("Musseline de Mandioquinha " + " R$ 100,00");
+        listChildPratos.add("Green Curry de Camarões " + " R$ 120,00");
+        listChildPratos.add("Pirarucu " + " R$ 90,00");
+
+        List<String> listChildBebidas = new ArrayList<String>();
+        listChildBebidas.add("Coca-Cola (lata) " + " R$ 4,00");
+        listChildBebidas.add("Guaraná (lata) " + " R$ 4,00");
+        listChildBebidas.add("Caipirinha " + " R$ 15,00");
+        listChildBebidas.add("Mojito " + " R$ 7,00");
+        listChildBebidas.add("Vinho Tinto " + " R$ 35,00");
+        listChildBebidas.add("Whisky Red Label 8 anos " + " R$ 100,00");
+
+        listChild.put(listGroup.get(0), listChildPratos);
+        listChild.put(listGroup.get(1), listChildBebidas);
+    }
+}
---
+++ HotelApp/app/src/main/java/br/metodista/hotelapp/adapter/ExpandableListAdapter.java
@@ -0,0 +1,108 @@
+package br.metodista.hotelapp.adapter;
+
+import java.util.HashMap;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import br.metodista.hotelapp.R;
+
+/**
+ * Created by Gustavo Assalin on 24/09/2015.
+ */
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private Context _context;
+    private List<String> listGroup; // header titles
+    // child data in format of header title, child title
+    private HashMap<String, List<String>> listChild;
+
+    public ExpandableListAdapter(Context context, List<String> listGroup,
+                                 HashMap<String, List<String>> listChild) {
+        this._context = context;
+        this.listGroup = listGroup;
+        this.listChild = listChild;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+        return this.listChild.get(this.listGroup.get(groupPosition)).get(childPosititon);
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+
+        final String childText = (String) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.child, null);
+        }
+
+        TextView txtChildNome = (TextView) convertView.findViewById(R.id.childTextNome);
+        TextView txtChildPreco = (TextView) convertView.findViewById(R.id.childTextPreco);
+
+        txtChildNome.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this.listChild.get(this.listGroup.get(groupPosition))
+                .size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.listGroup.get(groupPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this.listGroup.size();
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.group, null);
+        }
+
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.groupText);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
