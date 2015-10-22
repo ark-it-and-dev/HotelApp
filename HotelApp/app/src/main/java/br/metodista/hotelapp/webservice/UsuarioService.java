db0e6d67257fbcf5e190ff5d4b7c254e41fb5c5c
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-10-22 09:11:47 -0200

    + Adicao da classe que cuida do WebService dos usuarios

---
+++ HotelApp/app/src/main/java/br/metodista/hotelapp/webservice/UsuarioService.java
@@ -0,0 +1,46 @@
+package br.metodista.hotelapp.webservice;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import br.metodista.hotelapp.model.Usuario;
+
+/**
+ * Created by Gustavo Assalin on 22/10/2015.
+ */
+public class UsuarioService {
+
+    private static final String URL = "https://openws.herokuapp.com/hotelapp-usuarios";
+    private static final String API_KEY = "b4111c92d8a0f3d61f3cfd87e9a4eb75";
+
+    public List<Usuario> getAll() {
+        List<Usuario> usuarios = new ArrayList<>();
+        HttpURLConnection urlConnection = null;
+
+        try {
+            java.net.URL url = new URL(URL + API_KEY);
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in =  new BufferedInputStream(urlConnection.getInputStream());
+            Scanner s = new Scanner(in);
+            String conteudo = s.useDelimiter("\\A").next();
+
+            Gson gson = new Gson();
+            usuarios = gson.fromJson(conteudo, new TypeToken<List<Usuario>>(){}.getType());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            urlConnection.disconnect();
+        }
+
+        return usuarios;
+    }
+}
