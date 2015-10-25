da1fe6791bd94a870ccbe2ac5adb400ab7d1fd4b
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-10-25 20:40:04 -0200

    * Adaptacao para comunicacao com webservice

--- HotelApp/app/src/main/java/br/metodista/hotelapp/activities/Servicos.java
+++ HotelApp/app/src/main/java/br/metodista/hotelapp/activities/Servicos.java
@@ -4,33 +4,44 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.ExpandableListView.OnGroupClickListener;
 import android.widget.ExpandableListView.OnGroupCollapseListener;
 import android.widget.ExpandableListView.OnGroupExpandListener;
-import android.widget.ImageView;
-import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 
 import br.metodista.hotelapp.R;
 import br.metodista.hotelapp.adapter.ExpandableListAdapter;
+import br.metodista.hotelapp.model.Produto;
+import br.metodista.hotelapp.model.Usuario;
+import br.metodista.hotelapp.webservice.ProdutoService;
 
 public class Servicos extends AppCompatActivity {
+    //URL to get JSON Array
+
+    protected ProdutoService service = new ProdutoService();
+    protected List<Produto> listaProdutos;
+
     private ExpandableListAdapter adapter;
     private ExpandableListView expListView;
     private List<String> listGroup;
     private HashMap<String, List<String>> listChild;
 
     private Intent irPara;
-//    private Button btnCarrinho;
-//    private ImageView imgAdd;
+
+    private void carregarDados() {
+//        new CarregarProdutos().execute();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,16 +49,7 @@ public class Servicos extends AppCompatActivity {
         setContentView(R.layout.activity_servivos);
 
         // get the listview
-        expListView = (ExpandableListView) findViewById(R.id.expList);
-//        btnCarrinho = (Button) findViewById(R.id.btnListaCarrinho);
-//        btnCarrinho.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                irPara = new Intent(Servicos.this, Carrinho.class);
-//                startActivity(irPara);
-//            }
-//        });
-//        imgAdd = (ImageView) findViewById(R.id.imgAdd);
+        expListView = (ExpandableListView) findViewById(R.id.expListServicos);
 
         // preparing list data
         prepareListData();
@@ -74,9 +76,9 @@ public class Servicos extends AppCompatActivity {
 
             @Override
             public void onGroupExpand(int groupPosition) {
-                Toast.makeText(getApplicationContext(),
-                        listGroup.get(groupPosition) + " Expanded",
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(),
+//                        listGroup.get(groupPosition) + " Expanded",
+//                        Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -85,9 +87,9 @@ public class Servicos extends AppCompatActivity {
 
             @Override
             public void onGroupCollapse(int groupPosition) {
-                Toast.makeText(getApplicationContext(),
-                        listGroup.get(groupPosition) + " Collapsed",
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(),
+//                        listGroup.get(groupPosition) + " Collapsed",
+//                        Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -98,47 +100,68 @@ public class Servicos extends AppCompatActivity {
             @Override
             public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
                 // TODO Auto-generated method stub
-                Toast.makeText(
-                        getApplicationContext(),
-                        listGroup.get(groupPosition) + " : " + listChild.get(listGroup.get(groupPosition)).get(childPosition),
-                        Toast.LENGTH_SHORT).show();
+//                Toast.makeText(
+//                        getApplicationContext(),
+//                        listGroup.get(groupPosition) + " : " + listChild.get(listGroup.get(groupPosition)).get(childPosition),
+//                        Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        carregarDados();
+    }
+
     /*
      * Preparing the list data
-     */
+    */
     private void prepareListData() {
         listGroup = new ArrayList<String>();
         listChild = new HashMap<String, List<String>>();
 
-        listGroup.add("Pratos");
-        listGroup.add("Bebidas");
-
         List<String> listChildPratos = new ArrayList<String>();
-        listChildPratos.add("Ribeye 380g " + " R$ 250,00");
-        listChildPratos.add("Conflit de Pimentões " + " R$ 70,00");
-        listChildPratos.add("Trop Kobe Beef-Baby beef c batata soufflés " + " R$ 240,00");
-        listChildPratos.add("The Burguer Lab Star " + " R$ 200,00");
-        listChildPratos.add("Moqueca de Camarão " + " R$ 80,00");
-        listChildPratos.add("Lagosta ao Thermidor " + " R$ 190,00");
-        listChildPratos.add("Camarão a Provençal " + " R$ 150,00");
-        listChildPratos.add("Musseline de Mandioquinha " + " R$ 100,00");
-        listChildPratos.add("Green Curry de Camarões " + " R$ 120,00");
-        listChildPratos.add("Pirarucu " + " R$ 90,00");
-
         List<String> listChildBebidas = new ArrayList<String>();
-        listChildBebidas.add("Coca-Cola (lata) " + " R$ 4,00");
-        listChildBebidas.add("Guaraná (lata) " + " R$ 4,00");
-        listChildBebidas.add("Caipirinha " + " R$ 15,00");
-        listChildBebidas.add("Mojito " + " R$ 7,00");
-        listChildBebidas.add("Vinho Tinto " + " R$ 35,00");
-        listChildBebidas.add("Whisky Red Label 8 anos " + " R$ 100,00");
+
+        for(Produto produto : listaProdutos) {
+            switch (produto.getCategoria().toString()) {
+                case "PRATO":
+                    listChildPratos.add(produto.getNome() + "\n R$" + produto.getPreco());
+                    break;
+
+                case "BEBIDA":
+                    listChildBebidas.add(produto.getNome() + "\n R$" + produto.getPreco());
+                    break;
+            }
+        }
 
         listChild.put(listGroup.get(0), listChildPratos);
         listChild.put(listGroup.get(1), listChildBebidas);
+    }
+
+    private void createListGroup(String grupo) {
+        listGroup = new ArrayList<String>();
+
+        if(grupo != null) {
+            listGroup.add(grupo);
+        }
+    }
+
+    private void createListChild(String child, String tituloGrupo) {
+        listChild = new HashMap<String, List<String>>();
+
+        List<String> listChildren;
+
+        if(child != null) {
+            listChildren = new ArrayList<String>();
+
+            listChildren.add(child);
+
+            listChild.put(listGroup.get(listGroup.indexOf(tituloGrupo)), listChildren);
+        }
     }
 
     @Override
@@ -156,13 +179,78 @@ public class Servicos extends AppCompatActivity {
         int id = item.getItemId();
 
         switch (id) {
-            case R.id.action_contactar:
-                irPara = new Intent(Servicos.this, Contato.class);
-                startActivity(irPara);
+            case R.id.action_financeiro:
+                onDestroy();
 
+                irPara = new Intent(this, Financeiro.class);
+                startActivity(irPara);
+                break;
+
+            case R.id.action_checkout:
+                onDestroy();
+
+                irPara = new Intent(this, Checkout.class);
+                startActivity(irPara);
+                break;
+
+            case R.id.action_contactar:
+                onDestroy();
+
+                irPara = new Intent(this, Contato.class);
+                startActivity(irPara);
+                break;
+
+            case R.id.action_sair:
+                AlertDialog.Builder builder = new AlertDialog.Builder(Servicos.this);
+                builder.setTitle("Confirmação de Logoff");
+                builder.setMessage("Deseja realmente realizar o logoff?");
+                builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Usuario.encerrarSessao();
+
+                        onDestroy();
+
+                        irPara = new Intent(Servicos.this, Abertura.class);
+                        startActivity(irPara);
+                    }
+                });
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {}
+                });
+                AlertDialog alerta = builder.create();
+                alerta.show();
                 break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class CarregarProdutos extends AsyncTask<String, Void, List<Produto>> {
+
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = new ProgressDialog(Servicos.this);
+            progressDialog.show();
+        }
+
+        @Override
+        protected List<Produto> doInBackground(String... params) {
+            return service.getAll();
+        }
+
+        @Override
+        protected void onPostExecute(List<Produto> produtos) {
+            super.onPostExecute(produtos);
+
+            listaProdutos = produtos;
+
+            progressDialog.dismiss();
+        }
     }
 }
