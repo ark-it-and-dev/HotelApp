5b483ff4152461d621ffec722f4ece98d35540c5
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-11-03 20:49:57 -0200

    * Correcao e finalizaçcao activity Financeiro

--- HotelApp/app/src/main/java/br/metodista/hotelapp/activities/Financeiro.java
+++ HotelApp/app/src/main/java/br/metodista/hotelapp/activities/Financeiro.java
@@ -1,6 +1,5 @@
 package br.metodista.hotelapp.activities;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -30,6 +29,8 @@ public class Financeiro extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.setTitle("Financeiro");
 
         new CarregarConta().execute();
     }
@@ -82,85 +83,87 @@ public class Financeiro extends AppCompatActivity {
 
         @Override
         protected List<Usuario> doInBackground(String... params) {
-            service.getAll();
-
-            return null;
+            return service.getAll();
         }
 
         @Override
         protected void onPostExecute(List<Usuario> usuarios) {
             super.onPostExecute(usuarios);
 
-            for(Usuario usuario : usuarios) {
-                if(usuario.getLogin().equals(UsuarioLogado.getInstance().getLogin()) && usuario.getSenha().equals(UsuarioLogado.getInstance().getSenha())) {
-                    for(Produto p : usuario.getListaProduto()) {
-                        valorTotal += p.getPreco();
+            dialog.dismiss();
 
-                        listaConsumo.add(p);
+            if (usuarios == null) {
+                Toast.makeText(Financeiro.this, "Toast", Toast.LENGTH_SHORT).show();
+            } else {
+                for (Usuario usuario : usuarios) {
+                    if (usuario.getLogin().equals(UsuarioLogado.getInstance().getLogin()) && usuario.getSenha().equals(UsuarioLogado.getInstance().getSenha())) {
+                        for (Produto p : usuario.getListaProduto()) {
+                            valorTotal += p.getPreco();
+
+                            listaConsumo.add(p);
+                        }
+
+                        break;
                     }
                 }
 
-                return;
-            }
+                ScrollView scroll = new ScrollView(Financeiro.this);
 
-            ScrollView scroll = new ScrollView(Financeiro.this);
+                LinearLayout layoutGeral = new LinearLayout(Financeiro.this);
+                layoutGeral.setOrientation(LinearLayout.VERTICAL);
 
-            LinearLayout layoutGeral = new LinearLayout(Financeiro.this);
-            layoutGeral.setOrientation(LinearLayout.VERTICAL);
+                TextView txtDetalhes = new TextView(Financeiro.this);
+                txtDetalhes.setText("Detalhes:");
+                txtDetalhes.setTextSize(17);
 
-            LinearLayout layout = new LinearLayout(Financeiro.this);
-            layout.setOrientation(LinearLayout.HORIZONTAL);
+                layoutGeral.addView(txtDetalhes);
 
-            TextView txtValorTotal = new TextView(Financeiro.this);
-            txtValorTotal.setText("Valor Total de Consumo:");
-            txtValorTotal.setTextSize(17);
+                if (listaConsumo != null) {
+                    for (Produto produto : listaConsumo) {
+                        TextView nome = new TextView(Financeiro.this);
+                        nome.setText(produto.getNome());
+                        nome.setTextSize(17);
 
-            TextView txtTotal = new TextView(Financeiro.this);
-            txtTotal.setText("R$" + valorTotal);
-            txtTotal.setTextSize(17);
+                        TextView preco = new TextView(Financeiro.this);
+                        preco.setText("R$" + String.format("%.2f", produto.getPreco()));
+                        preco.setTextSize(15);
 
-            layout.addView(txtValorTotal);
-            layout.addView(txtTotal);
+                        TextView separador = new TextView(Financeiro.this);
+                        separador.setText("___________________________________________________________");
 
-            layoutGeral.addView(layout);
+                        layoutGeral.addView(nome);
+                        layoutGeral.addView(preco);
+                        layoutGeral.addView(separador);
+                    }
+                } else {
+                    TextView msg = new TextView(Financeiro.this);
+                    msg.setText("Não há Consumo Registrado até o Momento");
+                    msg.setTextSize(25);
 
-            TextView txtDetalhes = new TextView(Financeiro.this);
-            txtValorTotal.setText("Detalhes:");
-            txtValorTotal.setTextSize(17);
-
-            layoutGeral.addView(txtDetalhes);
-
-            if(listaConsumo != null) {
-                for (Produto produto : listaConsumo) {
-                    TextView nome = new TextView(Financeiro.this);
-                    nome.setText(produto.getNome());
-                    nome.setTextSize(17);
-
-                    TextView preco = new TextView(Financeiro.this);
-                    preco.setText("R$" + String.valueOf(produto.getPreco()));
-                    preco.setTextSize(15);
-
-                    TextView separador = new TextView(Financeiro.this);
-                    separador.setText("___________________________________________________________");
-
-                    layoutGeral.addView(nome);
-                    layoutGeral.addView(preco);
-                    layoutGeral.addView(separador);
+                    layoutGeral.addView(msg);
                 }
-            } else {
-                TextView msg = new TextView(Financeiro.this);
-                msg.setText("Não há Consumo Registrado até o Momento");
-                msg.setTextSize(25);
 
-                layoutGeral.addView(msg);
+                LinearLayout layout = new LinearLayout(Financeiro.this);
+                layout.setOrientation(LinearLayout.HORIZONTAL);
+
+                TextView txtValorTotal = new TextView(Financeiro.this);
+                txtValorTotal.setText("Valor Total de Consumo: ");
+                txtValorTotal.setTextSize(19);
+
+                layout.addView(txtValorTotal);
+
+                TextView txtTotal = new TextView(Financeiro.this);
+                txtTotal.setText(" R$" + String.format("%.2f", valorTotal));
+                txtTotal.setTextSize(19);
+
+                layout.addView(txtTotal);
+
+                layoutGeral.addView(layout);
+
+                scroll.addView(layoutGeral);
+
+                Financeiro.this.setContentView(scroll);
             }
-
-            scroll.addView(layoutGeral);
-
-            Financeiro.this.setContentView(scroll);
-
-            dialog.dismiss();
         }
     }
-
 }
