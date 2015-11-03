e5555ea9bdb4cc42dbc3124e63e52f0aaf1f017c
Gustavo Assalin <gustavo.assalin@hotmail.com>
2015-11-03 00:45:40 -0200

    * Adicao dos atributos privados _id e uma lista de produtos, mais getters e setters

--- HotelApp/app/src/main/java/br/metodista/hotelapp/model/Usuario.java
+++ HotelApp/app/src/main/java/br/metodista/hotelapp/model/Usuario.java
@@ -1,11 +1,31 @@
 package br.metodista.hotelapp.model;
 
+import java.util.List;
+
 /**
  * Created by Gustavo Assalin on 26/10/2015.
  */
 public class Usuario {
+    private String _id;
+    private List<Produto> listaProduto;
     private String login;
     private String senha;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public List<Produto> getListaProduto() {
+        return listaProduto;
+    }
+
+    public void setListaProduto(List<Produto> listaProduto) {
+        this.listaProduto = listaProduto;
+    }
 
     public String getLogin() {
         return login;
@@ -23,8 +43,4 @@ public class Usuario {
         this.senha = senha;
     }
 
-    @Override
-    public String toString() {
-        return login + " " + senha;
-    }
 }
